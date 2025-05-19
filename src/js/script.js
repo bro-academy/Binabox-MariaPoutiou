1c9a7d2eb914eb826ede5c6911a60da39e0021d4
MariaPoutiou <marypoutiou@gmai.com>
2025-05-19 14:01:39 +0300

    Fix js burger menu, spacing in hero section, blog and catalog

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const burgerBtn = document.querySelector('.header__burger-btn');
     const headerSection = document.querySelector('.header-section');
     const body = document.querySelector('body');
-    if (burgerBtn && headerSection) {
-      burgerBtn.addEventListener('click', function () {
-        headerSection.classList.toggle('is-shown');
-        body.classList.toggle('cropped');
-      });
-    }
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerBtn = document.querySelector('.header__burger-btn');
-    const headerSection = document.querySelector('.header-section');
-    const body = document.querySelector('body');
     const filterMobileBtnArea = document.querySelector('.catalog__filter-mobile-area');
     const asideCatalog = document.querySelector('.aside-section--catalog');
     if (burgerBtn && headerSection) {
